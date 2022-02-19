d8b0a504efe77658fbb8f4be5a0398ffbba9382a
fateme <fs.fatemi@chmail.ir>
2022-02-19 11:25:36 +0330

    clean code

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var binding: ActivityMainBinding
+    var mode = Mode.circle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,285 +14,39 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        var countinue = true
-        var mode = mode.circle
+        setListener()
+    }
+
+    fun setListener(){
         binding.button1.setOnClickListener{
-            if(buttonModeArray[1] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[1] = 0
-                    binding.button1.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[1] = 1
-                    binding.button1.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-                }
-                countinue = !isWin(1)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-
-                }
-
-            }
-
+            buttonSelected(1)
         }
-
         binding.button2.setOnClickListener {
-            if (buttonModeArray[2] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[2] = 0
-                    binding.button2.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[2] = 1
-                    binding.button2.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-                }
-                countinue = !isWin(2)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(2)
         }
-
         binding.button3.setOnClickListener {
-            if (buttonModeArray[3] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[3] = 0
-                    binding.button3.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[3] = 1
-                    binding.button3.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-                }
-                countinue = !isWin(3)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(3)
         }
-
         binding.button4.setOnClickListener {
-            if (buttonModeArray[4] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[4] = 0
-                    binding.button4.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[4] = 1
-                    binding.button4.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-
-                }
-                countinue = !isWin(4)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(4)
         }
-
         binding.button5.setOnClickListener {
-            if (buttonModeArray[5] == 3) {
-
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[5] = 0
-                    binding.button5.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[5] = 1
-                    binding.button5.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-
-                }
-                countinue = !isWin(5)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(5)
         }
-
         binding.button6.setOnClickListener {
-            if (buttonModeArray[6] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[6] = 0
-                    binding.button6.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[6] = 1
-                    binding.button6.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-
-                }
-                countinue = !isWin(6)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(6)
         }
-
         binding.button7.setOnClickListener {
-            if (buttonModeArray[7] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[7] = 0
-                    binding.button7.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[7] = 1
-                    binding.button7.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-
-                }
-                countinue = !isWin(7)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(7)
         }
-
         binding.button8.setOnClickListener {
-            if (buttonModeArray[8] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[8] = 0
-                    binding.button8.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[8] = 1
-                    binding.button8.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-
-                }
-                countinue = !isWin(8)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(9)
         }
-
         binding.button9.setOnClickListener {
-            if (buttonModeArray[9] == 3) {
-                if (mode == com.example.tictactoe.mode.circle) {
-                    buttonModeArray[9] = 0
-                    binding.button9.text = "O"
-                    mode = com.example.tictactoe.mode.cross
-
-                } else if (mode == com.example.tictactoe.mode.cross) {
-                    buttonModeArray[9] = 1
-                    binding.button9.text = "X"
-                    mode = com.example.tictactoe.mode.circle
-
-                }
-                countinue = !isWin(9)
-
-                if (countinue) {
-                    if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "Round O"
-                    } else if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "Round X"
-                    }
-                } else {
-                    if (mode == com.example.tictactoe.mode.cross) {
-                        binding.textView2.text = "O wiiiiin"
-                    } else if (mode == com.example.tictactoe.mode.circle) {
-                        binding.textView2.text = "X wiiiiin"
-                    }
-                }
-            }
+            buttonSelected(9)
         }
         binding.button10.setOnClickListener{
-             buttonModeArray = arrayListOf(3,3,3,3,3,3,3,3,3,3)
+            buttonModeArray = arrayListOf(3,3,3,3,3,3,3,3,3,3)
             binding.button1.text=""
             binding.button2.text=""
             binding.button3.text=""
@@ -303,9 +57,8 @@ class MainActivity : AppCompatActivity() {
             binding.button8.text=""
             binding.button9.text=""
             binding.textView2.text = "Round O"
-            mode = com.example.tictactoe.mode.circle
+            mode = com.example.tictactoe.Mode.circle
         }
-
     }
 
     var buttonModeArray : ArrayList<Int> = arrayListOf(3,3,3,3,3,3,3,3,3,3)
@@ -341,7 +94,6 @@ class MainActivity : AppCompatActivity() {
                  if(buttonModeArray[3] == buttonModeArray[5] && buttonModeArray[3] == buttonModeArray[7]){
                      mode = true
                  }
-
              }
 
              4 ->{
@@ -382,7 +134,6 @@ class MainActivity : AppCompatActivity() {
                  if(buttonModeArray[7] == buttonModeArray[5] && buttonModeArray[7] == buttonModeArray[3]){
                      mode = true
                  }
-
              }
 
              8 -> {if (buttonModeArray[8] == buttonModeArray[2] && buttonModeArray[8] == buttonModeArray[5]){
@@ -410,13 +161,48 @@ class MainActivity : AppCompatActivity() {
         return mode
     }
 
+    fun drawNewButton(bottunNum :Int, mode : Mode){
+            if (mode == com.example.tictactoe.Mode.circle) {
+                buttonModeArray[bottunNum] = 0
+                binding.button9.text = "O"
+            } else if (mode == com.example.tictactoe.Mode.cross) {
+                buttonModeArray[bottunNum] = 1
+                binding.button9.text = "X"
+            }
+        }
 
+    fun printRound(mode : Mode) : Mode{
+        if (mode == com.example.tictactoe.Mode.circle) {
+            binding.textView2.text = "Round O"
+            return com.example.tictactoe.Mode.cross
+        } else if (mode == com.example.tictactoe.Mode.cross) {
+            binding.textView2.text = "Round X"
+            return com.example.tictactoe.Mode.circle
+        }
+        return com.example.tictactoe.Mode.cross
+    }
 
+    fun printIfPlayerWin(buttonNum: Int, mode: Mode){
+            if (mode == com.example.tictactoe.Mode.cross) {
+                binding.textView2.text = "O wiiiiin"
+            } else if (mode == com.example.tictactoe.Mode.circle) {
+                binding.textView2.text = "X wiiiiin"
+            }
+    }
 
-
+    fun buttonSelected(num : Int){
+        if (buttonModeArray[num] == 3) {
+            drawNewButton(num, mode)
+            if(!isWin(num)){
+                mode = printRound(mode)
+            }else{
+                printIfPlayerWin(num, mode)
+            }
+        }
+    }
 }
 
-enum class mode{
+enum class Mode{
     cross,
     circle
 }
